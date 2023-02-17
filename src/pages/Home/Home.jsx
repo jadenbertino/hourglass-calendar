6d5cc577ed4913518a50b88383bd91c6af756ad6
Jaden Bertino <jaden@bertinofamily.com>
2023-02-16 19:49:12 -0800

    user must sign in before creating new events

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 // components
 import { NewEventModal, Sidebar } from '../../components/components'
@@ -8,11 +9,16 @@ import './Home.css'
 
 export default function Home() {
   const [modalActive, setModalActive] = useState('')
+  const { user } = useAuthContext()
 
   return (
     <div>
-      <Sidebar setModalActive={setModalActive} />
-      {modalActive === "newEvent" && <NewEventModal setModalActive={setModalActive}/> }
+      {user ? (<>
+        <Sidebar setModalActive={setModalActive} />
+        {modalActive === "newEvent" && <NewEventModal setModalActive={setModalActive}/> }
+      </>) : (<>
+        <p className="container sidebar">please sign in</p>
+      </>)}
     </div>
   )
 }
