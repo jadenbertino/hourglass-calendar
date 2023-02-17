896432b01e2114ac4277c885fa9706f8b10d1aac
Jaden Bertino <jaden@bertinofamily.com>
2023-02-16 18:47:43 -0800

    finish up basic new event form validation

--- src/components/NewEventModal.jsx
+++ src/components/NewEventModal.jsx
@@ -11,78 +11,136 @@ import './NewEventModal.css'
 
 export default function NewEventModal({setModalActive}) {
   // form controls
-  const [name, setName] = useState('')
-  const [date, setDate] = useState('')
-  const [startTime, setStartTime] = useState('')
-  const [endTime, setEndTime] = useState('')
-  const [notes, setNotes] = useState('')
+  const [eventName, setEventName] = useState('')
+  const [eventDate, setEventDate] = useState('')
+  const [eventStartTime, setEventStartTime] = useState('')
+  const [eventEndTime, setEventEndTime] = useState('')
+  const [eventNotes, setEventNotes] = useState('')
 
   // form validation
+  const [validName, setValidName] = useState(true)
+  const [validNotes, setValidNotes] = useState(true)
+  const [validDate, setValidDate] = useState(true)
+  const [validStartTime, setValidStartTime] = useState(true)
+  const [validEndTime, setValidEndTime] = useState(true)
   
   const { user } = useAuthContext()
 
-  function resetFields() {
-    setName('')
-    setDate('')
-    setStartTime('')
-    setEndTime('')
-    setNotes('')
+  /*
+
+    form validation
+
+  */
+
+  function resetValidation() {
+    setValidName(true)
+    setValidNotes(true)
+    setValidStartTime(true)
+    setValidEndTime(true)
   }
 
-  async function createEvent(e) {
+  function validateFormControls(e) {
     e.preventDefault()
+
+    resetValidation()
+    let allFieldsAreValid = true
+    // name + notes can't be empty
+    if (eventName.length === 0) {
+      setValidName(false)
+      allFieldsAreValid = false
+    }
+    if (eventNotes.length === 0) {
+      setValidNotes(false)
+      allFieldsAreValid = false
+    }
+    // date must be 10 chars (YYYY-MM-DD)
+    if (eventDate.length !== 10) {
+      setValidDate(false)
+      allFieldsAreValid = false
+    }
+    // start time & end time must be in HH:MM:XM format, thank you chatgpt
+    const timeRegex = /^(1[0-2]|0?[1-9])(:[0-5][0-9])?\s?[AP]M$/i;
+    if (!timeRegex.test(eventStartTime)) {
+      setValidStartTime(false)
+      allFieldsAreValid = false
+    }
+    if (!timeRegex.test(eventEndTime)) {
+      setValidEndTime(false)
+      allFieldsAreValid = false
+    }
+
+    if (allFieldsAreValid) createEvent()
+  }
+  /*
+
+    event creation
+
+  */
+
+  function resetFields() {
+    setEventName('')
+    setEventDate('')
+    setEventStartTime('')
+    setEventEndTime('')
+    setEventNotes('')
+  }
+
+
+  async function createEvent() {
+    /*
+      Create Event
+    */
     const event = {
-      name,
-      notes,
-      date,
-      startTime,
-      endTime,
+      eventName,
+      eventNotes,
+      eventDate,
+      eventStartTime,
+      eventEndTime,
       uid: user.uid
     };
     await addDoc(collection(db, "events"), event);
-    resetFields()
+    setModalActive('')
   }
 
   return (
     <Modal>
-      <form className='new-event-form' onSubmit={createEvent}>
+      <form className='new-event-form' onSubmit={validateFormControls}>
         <input
-          className='name'
+          className={`name ${validName ? "" : "invalid"}`}
           type="text"
           placeholder='Event Name' 
-          onChange={(e) => setName(e.target.value)}
-          value={name}
+          onChange={(e) => setEventName(e.target.value)}
+          value={eventName}
         />
         <input
-          className='date'
+          className={`date ${validDate ? "" : "invalid"}`}
           type="date"
           placeholder = 'Date'
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          onChange={(e) => setEventDate(e.target.value)}
+          value={eventDate}
         />
         <div className="times-wrapper">
           <input
-            className='time'
+            className={`time ${validStartTime ? "" : "invalid"}`}
             type="text"
             placeholder='Start Time'
-            onChange={(e) => setStartTime(e.target.value)}
-            value={startTime}
+            onChange={(e) => setEventStartTime(e.target.value)}
+            value={eventStartTime}
           />
           <i className="fa-solid fa-arrow-right"></i>
           <input
-            className='time'
+            className={`time ${validEndTime ? "" : "invalid"}`}
             type="text"
             placeholder='End Time'
-            onChange={(e) => setEndTime(e.target.value)}
-            value={endTime}
+            onChange={(e) => setEventEndTime(e.target.value)}
+            value={eventEndTime}
           />
         </div>
-
         <textarea
-          className='notes'
+          className={`notes ${validNotes ? "" : "invalid"}`}
           placeholder='Event Notes'
-          onChange={(e) => setNotes(e.target.value)}
-          value={notes}
+          onChange={(e) => setEventNotes(e.target.value)}
+          value={eventNotes}
         />
         <button className='btn cancel-btn' type="button" onClick={(() => setModalActive(''))}>Cancel</button>
         <button className='btn save-btn'>Save</button>
