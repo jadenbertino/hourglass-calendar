f7d20adfe81404a7d1daaf162eb4ea7aa82d7783
Jaden Bertino <jaden@bertinofamily.com>
2023-02-23 10:09:02 -0800

    create & implement formatDate function in date context

--- src/context/DateContext.js
+++ src/context/DateContext.js
@@ -4,9 +4,17 @@ export const DateContext = createContext();
 
 export function DateContextProvider({ children }) {
   const [dateContext, setDateContext] = useState(new Date());
+  
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+    return formattedDate
+  }
 
   return (
-    <DateContext.Provider value={{ dateContext, setDateContext }}>
+    <DateContext.Provider value={{ dateContext, setDateContext, formatDate }}>
       {children}
     </DateContext.Provider>
   );
--- src/pages/DailyView/DailyView.jsx
+++ src/pages/DailyView/DailyView.jsx
@@ -10,23 +10,26 @@ import './DailyView.css'
 
 export default function DailyView() {
   const { user } = useAuthContext()
-  const { dateContext, setDateContext } = useDateContext()
-  const [dayName, setDayName] = useState('')
-  const [dayOfMonth, setDayOfMonth] = useState('')
+  const { dateContext, setDateContext, formatDate } = useDateContext()
   const {modalContext} = useModalContext()
 
-    // if user isn't signed in redirect to signin / signup page
-    const nav = useNavigate()
-    useEffect(() => {
-      if (!user) {
-        nav('/')
-      }
-    }, [user])
+  const [dayName, setDayName] = useState('')
+  const [dayOfMonth, setDayOfMonth] = useState('')
+  const [formattedDate, setFormattedDate] = useState('')
+
+  // if user isn't signed in redirect to signin / signup page
+  const nav = useNavigate()
+  useEffect(() => {
+    if (!user) {
+      nav('/')
+    }
+  }, [user])
 
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
   useEffect(() => {
     setDayName(days[dateContext.getDay()])
     setDayOfMonth(dateContext.getDate())
+    setFormattedDate(formatDate(dateContext))
   }, [dateContext])
 
   function incrementDate() {
@@ -50,6 +53,7 @@ export default function DailyView() {
         <i className="fa-solid fa-angle-right"></i>
       </button>
     </Nav>
+
     <main>
       <Sidebar/>
       <div className="day-of-month">
