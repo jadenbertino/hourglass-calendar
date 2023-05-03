bf1a472869e8104141b7ce278766c90fb30c8a60
Jaden Bertino <jaden@bertinofamily.com>
2023-05-03 11:19:23 -0700

    change firebase project name to 'hourglass-calendar'

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,20 +1,16 @@
-// Import the functions you need from the SDKs you need
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBLO23uwJKUcAbGEunt78JWpS5XAMf6Ghs",
-  authDomain: "calendar-b8161.firebaseapp.com",
-  projectId: "calendar-b8161",
-  storageBucket: "calendar-b8161.appspot.com",
-  messagingSenderId: "426904028039",
-  appId: "1:426904028039:web:3974d20adb96d4c0744922"
+  apiKey: "AIzaSyAkcDWebKzJwzlZJQsB4NO08RHVySfkqeo",
+  authDomain: "hourglass-calendar.firebaseapp.com",
+  projectId: "hourglass-calendar",
+  storageBucket: "hourglass-calendar.appspot.com",
+  messagingSenderId: "617927885464",
+  appId: "1:617927885464:web:4c629a3b793946c3c086ed"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const db = getFirestore(app);
